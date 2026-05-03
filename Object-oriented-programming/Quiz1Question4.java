f7cc66e86829febed0495a6c47895030d5397662
chhorponnsovan <chhorponn999@gmail.com>
2026-05-03 17:05:23 +0000

    Auto commit

--- Object-oriented-programming/Quiz1Question4.java
+++ Object-oriented-programming/Quiz1Question4.java
@@ -10,12 +10,12 @@ public class Quiz1Question4 {
         hours = sc.nextInt(); // Prompt the user to input hours and read the input using the Scanner object, storing it in the variable hours.
         System.out.print("Please input minutes: ");
         minutes = sc.nextInt();
-        System.out.print("[Please input seconds: ]");
+        System.out.print("Please input seconds: ");
         seconds = sc.nextInt();
        
         totalSeconds = (hours * 3600) + (minutes * 60) + seconds; // Calculate the total number of seconds by multiplying hours by 3600, minutes by 60, and adding all three components together.
 
-        System.out.printf("Number of seconds = %dx3600 + %dx60 + %d = %d\n", hours, minutes, seconds, totalSeconds); 
+        System.out.printf("Number of seconds = %dx3600 + %dx60 + %d = %d seconds\n", hours, minutes, seconds, totalSeconds); 
         // Format the output to display the calculation of total seconds, showing the contribution of hours, minutes, and seconds in the format "Number of seconds = Hx3600 + Mx60 + S = TotalSeconds", where H, M, S, and TotalSeconds are replaced with their respective values.
         sc.close();
     }
