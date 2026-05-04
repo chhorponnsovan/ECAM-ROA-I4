c3400184448a088c80e105705e48235c7f898a74
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 10:20:32 +0000

    Auto commit

--- Object-oriented-programming/QuizQuestion/Quiz3Question11.java
+++ Object-oriented-programming/QuizQuestion/Quiz3Question11.java
@@ -1,5 +1,34 @@
 package QuizQuestion;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Quiz3Question11 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int targetNumber = random.nextInt(100) + 1;
+        int attempts = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have selected a number between 1 and 100. Can you guess it?");
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int guess = scanner.nextInt();
+            attempts++; // attempts++
+
+            if (guess < targetNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > targetNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the number " + targetNumber + " in " + attempts + " attempts.");
+            }
+        }
+
+        scanner.close();
+    }
 }
