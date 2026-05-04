4b3dc9fe5a65bce65d1bdf9939ec5704d84f20e6
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 09:54:42 +0000

    Auto commit

--- Object-oriented-programming/QuizQuestion/Quiz3Question2.java
+++ Object-oriented-programming/QuizQuestion/Quiz3Question2.java
@@ -1,5 +1,21 @@
 package QuizQuestion;
+// speed operation given distance and time
+import java.util.Scanner;
 
 public class Quiz3Question2 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the distance (in kilometers): ");
+        double distance = scanner.nextDouble();
+        System.out.print("Enter the time (in hours): ");
+        double time = scanner.nextDouble();
+
+        if (time == 0) {
+            System.out.println("Time must be greater than zero.");
+        } else {
+            double speed = distance / time;
+            System.out.printf("The speed is: %.2f km/h\n", speed);
+        }
+        scanner.close();
+    }
 }
