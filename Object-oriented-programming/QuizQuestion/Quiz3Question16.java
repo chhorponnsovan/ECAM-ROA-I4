6a27deccb891f02a4511c6544e95e37f4f5a1dae
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 10:34:41 +0000

    Auto commit

--- Object-oriented-programming/QuizQuestion/Quiz3Question16.java
+++ Object-oriented-programming/QuizQuestion/Quiz3Question16.java
@@ -1,5 +1,19 @@
 package QuizQuestion;
 
+//sum all even from 1 to given
+import java.util.Scanner;
 public class Quiz3Question16 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        
+        for (int i = 2; i <= n; i += 2) {
+            sum += i; // sum += i;
+        }
+        System.out.println("Sum of even numbers from 1 to " + n + " is: " + sum);
+        scanner.close();
+    }
 }
