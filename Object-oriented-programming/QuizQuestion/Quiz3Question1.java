109186ff16a6aad6499a27f1be6422392eef6fce
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 09:42:15 +0000

    Auto commit

---
+++ Object-oriented-programming/QuizQuestion/Quiz3Question1.java
@@ -0,0 +1,50 @@
+package QuizQuestion;
+//Calculator of two numbers
+import java.util.Scanner;
+public class Quiz3Question1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.println("Choose operation: ");
+        System.out.println("1. Addition");    
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int choice = scanner.nextInt(); // Read the user's choice for the operation
+        double result = 0;
+        boolean validoperation = true;
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                System.out.printf("Result: %.2f\n", result);
+                break;
+            case 2:
+                result = num1 - num2;
+                System.out.printf("Result: %.2f\n", result);
+                break;
+            case 3:
+                result = num1 * num2;
+                System.out.printf("Result: %.2f\n", result);
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.printf("Result: %.2f\n", result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Invalid choice. Please select a valid operation.");
+        }
+
+        scanner.close();
+    }
+}
