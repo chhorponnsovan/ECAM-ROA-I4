de2b1fa6be29131d8c1af800a310fda0e852a35d
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 08:38:59 +0000

    Auto commit

---
+++ Object-oriented-programming/Quiz2Question2.java
@@ -0,0 +1,11 @@
+// display all odd numbers from 1 to 500
+public class Quiz2Question2 {
+        public static void main(String[] args) {
+        int runner = 1;
+        int n = 500;
+        while(runner < n){
+                System.out.printf("%d ", runner);
+            runner += 2;         
+        }
+    }
+}
