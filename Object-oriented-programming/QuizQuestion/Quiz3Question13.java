c92fda9180a5c491cbb77ddd571ddbbec43450c1
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 10:24:48 +0000

    Auto commit

--- Object-oriented-programming/QuizQuestion/Quiz3Question13.java
+++ Object-oriented-programming/QuizQuestion/Quiz3Question13.java
@@ -1,5 +1,19 @@
 package QuizQuestion;
-
+// is odd or even
+import java.util.Scanner;
 public class Quiz3Question13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        
+        if (number % 2 == 0) { // number % 2 == 0
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+        
+        scanner.close();
+    }
     
 }
