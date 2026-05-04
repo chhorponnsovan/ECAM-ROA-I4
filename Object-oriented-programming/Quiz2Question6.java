6e45f582e780230973f19e7348183f83cd9555d9
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 09:06:46 +0000

    Auto commit

--- Object-oriented-programming/Quiz2Question6.java
+++ Object-oriented-programming/Quiz2Question6.java
@@ -1,3 +1,34 @@
+/* Write a Java program to display a triangle made from stars. The number of lines and number of columns is given by user. Example:
+
+*
+**
+***
+****
+*****
+*/
+
+import java.util.Scanner;
+
 public class Quiz2Question6 {
-    
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input number of lines: ");
+        int lines = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int columns = sc.nextInt();
+        int k;
+        if (lines>=columns){
+            k = lines;
+        } else {
+            k = columns;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
 }
