3f62f18733eb167522f828e87c47bf326d9c530e
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 10:36:30 +0000

    Auto commit

--- Object-oriented-programming/QuizQuestion/Quiz3Question17.java
+++ Object-oriented-programming/QuizQuestion/Quiz3Question17.java
@@ -1,5 +1,20 @@
 package QuizQuestion;
-
+// sum of first and second number 
+import java.util.Scanner;
 public class Quiz3Question17 {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        double sum = num1 + num2; // double sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+
+        scanner.close();
+    }
+
 }
