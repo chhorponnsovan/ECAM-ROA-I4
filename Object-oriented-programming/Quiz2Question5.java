4884caad3c6c5f63a24f9192034165e986f7ddde
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 09:05:15 +0000

    Auto commit

--- Object-oriented-programming/Quiz2Question5.java
+++ Object-oriented-programming/Quiz2Question5.java
@@ -1,3 +1,29 @@
+/* If we input 20 30 50 in sequences, the proram below will produce result as following:
+ *
+------- Program for converting time to seconds -------
+Please input hours: 20
+Please input minutes: 30
+Please input seconds: 50
+
+Number of seconds = 20x3600 + 30x60 + 50 = 73850
+    */
+import java.util.Scanner;
 public class Quiz2Question5 {
-    
+        public static void main(String[] args) {
+        System.out.println("------- Program for converting from seconds into hours:minutes:seconds ------- ");
+        Scanner sc = new Scanner(System.in);
+        int totalSeconds, hours, minutes, seconds; // Declare four integer variables, totalSeconds, hours, minutes, and seconds.
+        System.out.print("Please input hours: ");
+        hours = sc.nextInt(); // Prompt the user to input hours and read the input using the Scanner object, storing it in the variable hours.
+        System.out.print("Please input minutes: ");
+        minutes = sc.nextInt();
+        System.out.print("Please input seconds: "); // Prompt the user to input seconds and read the input using the Scanner object, storing it in the variable seconds.
+        seconds = sc.nextInt();
+       
+        totalSeconds = (hours * 3600) + (minutes * 60) + seconds; // Calculate the total number of seconds by multiplying hours by 3600, minutes by 60, and adding all three components together.
+
+        System.out.printf("Number of seconds = %dx3600 + %dx60 + %d = %d\n", hours, minutes, seconds, totalSeconds);
+        // Format the output to display the calculation of total seconds, showing the contribution of hours, minutes, and seconds in the format "Number of seconds = Hx3600 + Mx60 + S = TotalSeconds", where H, M, S, and TotalSeconds are replaced with their respective values.
+        sc.close();
+    }
 }
