0916f2d0abfa06b7997786555772e6eaee811216
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 10:07:56 +0000

    Auto commit

--- Object-oriented-programming/QuizQuestion/Quiz3Question7.java
+++ Object-oriented-programming/QuizQuestion/Quiz3Question7.java
@@ -1,5 +1,17 @@
 package QuizQuestion;
-
+// name + age print
+import java.util.Scanner;
 public class Quiz3Question7 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Hello " + name + ", you are " + age + " years old.");
+        scanner.close();
+    }
 }
