bb40d3052fea7bbce09b8b577ca92d793e99a075
chhorponnsovan <chhorponn999@gmail.com>
2026-05-04 10:11:52 +0000

    Auto commit

--- Object-oriented-programming/QuizQuestion/Quiz3Question8.java
+++ Object-oriented-programming/QuizQuestion/Quiz3Question8.java
@@ -1,5 +1,20 @@
 package QuizQuestion;
 
-public class Quiz3Question8 {
-    
+import java.util.Scanner;
+    public class Quiz3Question8 {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine(); // nextLine()
+       
+        int wordCount = 0;
+        if(input != null && !input.trim().isEmpty() ){ // isEmpty()
+            String[] words = input.trim().split("\\s+"); // split("\\s+")
+            wordCount = words.length; // wordCount
+        }
+        System.out.println("Number of words: " + wordCount); // wordCount
+       
+        scanner.close(); // close()
+    }
 }
